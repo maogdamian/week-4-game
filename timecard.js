95413f11d51ee3d12c44ed6bd80c69575bb9dc06
Damian Bennett <maogdamian@gmail.com>
2018-04-05 19:04:20 -0700

    updated css

---
+++ timecard.js
@@ -0,0 +1,15 @@
+// 2. Button for adding Employees
+$("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+  // Grabs user input
+  var empName = $("#employee-name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+  // Creates local "temporary" object for holding employee data
+  var newEmp = {
+    name: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate
+  };
